9747b34daf784c64bcef8f9744cf2b01ca2f5f2b
mhinkie <34032605+mhinkie@users.noreply.github.com>
2018-07-16 13:02:04 +0200

    added new def_gw_mac to finish network configuration

--- research/capturing_forwarding/libtins_detector/host_config/router_nat.sh
+++ research/capturing_forwarding/libtins_detector/host_config/router_nat.sh
@@ -6,7 +6,16 @@ EXTERNAL_IF=wlx2824ff1a05f9
 #interface facing the vms
 INTERNAL_IF=vboxnet0
 
+# network information of internal vm network (for routing configuration)
+VM_INTERNAL_NETWORK=192.168.100.0
+VM_INTERNAL_NETMASK=255.255.255.0
+#inteface on ooni detector facing the host
+OONI_DETECTOR_IP=192.168.56.101
 
+# activate nat
 echo "1" > /proc/sys/net/ipv4/ip_forward
 iptables -t nat -A POSTROUTING -o ${EXTERNAL_IF} -j MASQUERADE
 iptables -A FORWARD -i ${INTERNAL_IF} -j ACCEPT
+
+# add route to internal vm network
+route add -net ${VM_INTERNAL_NETWORK} netmask ${VM_INTERNAL_NETMASK} gw ${OONI_DETECTOR_IP}
